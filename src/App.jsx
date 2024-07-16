d34b2f9e39673f36f35609d970fb2b3d40b3ad48
akolle <andykoller55@yahoo.com>
2024-07-16 17:46:55 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import data from "./data";
+import { useState } from "react";
+
+const App = () => {
+  
+  const [list, setList] = useState(data);
+  
+  const clearAll = () => {
+    setList([]);
+  }
+
+  return (
+    <main>
+      <section className="container">
+        <h2 style={{margin: "50px"}}>There are {list.length} birthdays today.</h2>
+        {list.map((person) => {
+          const {name, age, image} = person;
+          return (
+            <div>
+              <h2>{name}</h2>
+              <h4>{age}</h4>
+              <img className='img' src={image}></img>   
+            </div>
+          )
+        })}
+        <button type="button" className="btn btn-block" onClick={clearAll}>clear all</button>
+      </section>
+    </main>
+  )
+  
+};
+export default App;
